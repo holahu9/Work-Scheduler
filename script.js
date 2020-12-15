72da43b6b6471f6d113bb468e8f3b24930bbab5e
Duc Dinh <holahu9@gmail.com>
2020-12-15 10:50:46 -0500

    update

--- script.js
+++ script.js
@@ -2,44 +2,44 @@ $(document).ready(function(){
 
     var hour = moment().hours();
 
-    //Moment.js date for top of page
+    
     function getDate(){
-        $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+        $("#currentDay").text(moment().format('MMMM Do, YYYY h:mm:ss'));
     };
-//Color coding schedule dependant upon hour
-function colorSchedule(){
-    $("input").each(function(){
-        var rowHour = $(this).attr("id");
-        var rowNumber = parseInt(rowHour);
-        if (rowNumber === hour){
-            $(this).addClass("present");
-        } else if (rowNumber < hour){
-            $(this).addClass("past");
-        } else {
-            $(this).addClass("future");
-        };
-    });
-};
 
-//Rendering stored inputs after page refresh
-function renderStoredInputs(){
-    $(".event").each(function(){
-        var inputId = $(this).attr("id");
-        $(this).val(localStorage.getItem(inputId));
-    });
-};
+  
+    function colorSchedule(){
+        $("input").each(function(){
+            var rowHour = $(this).attr("id");
+            var rowNumber = parseInt(rowHour);
+            if (rowNumber === hour){
+                $(this).addClass("today");
+            } else if (rowNumber < hour){
+                $(this).addClass("yesterday");
+            } else {
+                $(this).addClass("tomorrow");
+            };
+        });
+    };
 
-//Click event to save user input in local storage
-$(".saveBtn").click(function(){
-    var scheduleInputs = $(this).siblings(".event").val();
-    var inputsLocation = $(this).siblings(".event").attr("id");
-    localStorage.setItem(inputsLocation,scheduleInputs);
-});
-
-setInterval(getDate,1000);
-colorSchedule();
-setInterval(colorSchedule,1000);
-renderStoredInputs();
+  
+    function inputs(){
+        $(".event").each(function(){
+            var inputId = $(this).attr("id");
+            $(this).val(localStorage.getItem(inputId));
+        });
+    };
 
     
+    $(".save").click(function(){
+        var scheduleInputs = $(this).siblings(".event").val();
+        var inputsLocation = $(this).siblings(".event").attr("id");
+        localStorage.setItem(inputsLocation,scheduleInputs);
+    });
+    
+    setInterval(getDate,1000);
+    colorSchedule();
+    setInterval(colorSchedule,1000);
+    inputs();
+
 });
