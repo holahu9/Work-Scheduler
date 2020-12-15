84c1b0fb73aac7fa9bbb0e9bc91a442a8dcb487b
Duc Dinh <holahu9@gmail.com>
2020-12-15 09:18:23 -0500

    update

--- script.js
+++ script.js
@@ -6,6 +6,21 @@ $(document).ready(function(){
     function getDate(){
         $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
     };
+//Color coding schedule dependant upon hour
+function colorSchedule(){
+    $("input").each(function(){
+        var rowHour = $(this).attr("id");
+        var rowNumber = parseInt(rowHour);
+        if (rowNumber === hour){
+            $(this).addClass("present");
+        } else if (rowNumber < hour){
+            $(this).addClass("past");
+        } else {
+            $(this).addClass("future");
+        };
+    });
+};
+
 
     
 });
