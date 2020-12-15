840c62aec608f477b6edc102d5d85917892b9b14
Duc Dinh <holahu9@gmail.com>
2020-12-15 18:54:53 -0500

    update

--- asset/script.js
+++ asset/script.js
@@ -1,15 +1,15 @@
 
-// function to get the current date //
+
 $(document).ready(function(){
 
     var hour = moment().hours();
 
-    
+    // function to get the current date //
     function getDate(){
         $("#currentDate").text(moment().format('MMMM Do, h:mm:ss'));
     };
 
-  // function to assign the colors to present, future, and past.
+  // function to assign the colors dependant upon hour
     function color(){
         $("input").each(function(){
             var rowHour = $(this).attr("id");
@@ -24,14 +24,14 @@ $(document).ready(function(){
         });
     };
 
-  // save the input to local storage//
+  // save the user input to local storage after page refresh
     function inputs(){
         $(".event").each(function(){
             var inputId = $(this).attr("id");
             $(this).val(localStorage.getItem(inputId));
         });
     };
-
+ //Click event to save user input in local storage
     
     $(".save").click(function(){
         var scheduleInputs = $(this).siblings(".event").val();
